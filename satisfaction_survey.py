8adba71e04a0da1bfacababa2ed57812bf8c8728
devminsu1101 <devminsu1101@gmail.com>
2025-11-20 22:45:42 +0900

    Doc. dummy-03.xlsx 추가, satisfaction_survey.py 수정

--- satisfaction_survey.py
+++ satisfaction_survey.py
@@ -4,9 +4,10 @@ import re
 
 # 1. 변수 설정
 # 1-1. 불러올 파일 설정 
-file = "2025년 노란우산 고객지원 교육 만족도 조사 결과.xlsx"
+file = "dummy-03.xlsx"
 if not os.path.isfile(file) : 
   raise FileNotFoundError(f"{file} 파일 없음")
+column_1 = "과정 전반 만족도"
 # 1-2. 파일 내 각각의 시트에 접근
 sheets = pd.read_excel(file, sheet_name=None) # 파일 내 여러 시트를 읽고 싶다면! 'sheet_name=None'
 # 1-3. 이후 프린트할 결과 저장용 
@@ -24,11 +25,11 @@ for sheet_name, df in sheets.items():
     # 2-2. 1번 문항 이름 찾기 
     question1_col = None  
     for col in df.columns: 
-        if "1." in str(col) and "전반적인 만족도" in str(col): 
+        if "1." in str(col) and column_1 in str(col): 
             question1_col = col 
             break
     if question1_col is None : 
-        print("'1. 전반적인 만족도' 문항을 찾을 수 없습니다.")
+        print(f"'1. {column_1}' 문항을 찾을 수 없습니다.")
         continue 
     
     # 2-3. 요구사항 1) 차수별 1번 문항 만족도 조사 
@@ -60,8 +61,8 @@ for sheet_name, df in sheets.items():
 
 
     print("\n")
-    print(" 각 문항별 평균 만족도 : ")
+    print(" 각 문항별 평균 만족도 : \n")
     for col, value in means.items(): 
         print(f" - {col} : {value:.2f}")
 
-    print("\n\n")
+    print("\n")
